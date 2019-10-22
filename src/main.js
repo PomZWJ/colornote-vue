01d1ed0b557e96574d143298bd46a61260673c55
PomZWJ <zwj@19951123>
2019-10-22 11:46:23 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import animated from 'animate.css'
+import 'lib-flexible'
+Vue.use(animated);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
