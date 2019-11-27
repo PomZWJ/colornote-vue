54fc69f34d51efe15fa69195f7c895798ac6f47c
PomZWJ <zwj@19951123>
2019-11-27 15:16:44 +0800

    commit

--- src/page/login.vue
+++ src/page/login.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-      <span>123</span>
+      <div class="back-div">
+
+      </div>
     </div>
 </template>
 
@@ -11,5 +13,13 @@
 </script>
 
 <style scoped>
-
+  .back-div{
+    position: fixed;
+    top:0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: url("../../static/login_back.jpg");
+    background-size: 100% 100%;
+  }
 </style>
